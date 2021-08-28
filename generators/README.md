30aa93b41a3c14477eb06dbb6fea9a68d0f4c491
edgar-chan@ <edgar.chan@cloverhealth.com>
2021-08-28 15:01:31 +0800

    Basic iteration protocol

--- generators/README.md
+++ generators/README.md
@@ -6,3 +6,10 @@ Author: David Beazley
 Originally Presented at PyCon 2008
 Updated: October, 2018
 Link: http://www.dabeaz.com/generators/Generators.pdf
+
+### 1. Basic iteration protocol
+
+```shell
+python iteration.py
+5 4 3 2 1
+```
---
+++ generators/iteration.py
@@ -0,0 +1,26 @@
+class counterdown_iter:
+    count: int
+
+    def __init__(self, count):
+        self.count = count
+
+    def __next__(self) -> int:
+        if self.count <= 0:
+            raise StopIteration
+        r = self.count
+        self.count -= 1
+        return r
+
+class countdown:
+    start: int
+    
+    def __init__(self, start: int):
+        self.start = start
+
+    def __iter__(self):
+        return counterdown_iter(self.start)
+
+
+if __name__ == "__main__":
+    for i in countdown(5):
+        print(i, end=" ")
